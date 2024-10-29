808a7f1df76dc8169c2cc1e40c0312c809624190
sumdx <sumdas74@gmail.com>
2024-10-29 10:07:01 -0700

    Available button added

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,15 +1,32 @@
+import { FaUser } from "react-icons/fa";
+import { FaFlag } from "react-icons/fa6";
 
-const Player = (player) => {
-    console.log(player.name)
-    return (
+const Player = ({ player }) => {
+  return (
+    <div className="rounded-xl border border-gray-600 p-6">
+      <div className="rounded-xl">
+        <img
+          className="w-full rounded-xl h-48 object-cover"
+          src={player.image}
+          alt=""
+        />
+      </div>
+      <div className="mt-4 flex items-center gap-2">
+        <p className="text-xl" href="">
+          <FaUser></FaUser>
+        </p>
+        <h1 className="font-bold text-xl">{player.name}</h1>
+      </div>
+      <div className="mt-4 flex items-center gap-2">
         <div>
-            
-            <img src={player.player.image} alt="" />
-            <h1>{player.player.name}</h1>
-            
-            
+          <p className="text-lg text-gray-400" href="">
+            <FaFlag></FaFlag>
+          </p>
+          <p className="text-lg text-gray-400">{player.country}</p>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Player;
--- src/components/Players/Players.jsx
+++ src/components/Players/Players.jsx
@@ -2,22 +2,30 @@ import { useEffect, useState } from "react";
 import Player from "../Player/Player";
 
 const Players = () => {
+  const [players, addPlayers] = useState([]);
 
-    const [players, addPlayers ] = useState([]);
+  useEffect(() => {
+    fetch("players.json")
+      .then((res) => res.json())
+      .then((data) => addPlayers(data));
+  }, []);
 
-    useEffect(()=>{
-        fetch('players.json')
-        .then(res => res.json())
-        .then(data => addPlayers(data))
-    },[])
-
-    return (
+  return (
+    <div className="container mx-auto">
+      <div className="flex justify-between items-center my-8">
+        <h1 className="font-bold text-xl">Available Player</h1>
         <div>
-            {
-                players.map(player =><Player key={player.playerId} player ={player}></Player>)
-            }
+          <button className="rounded-l-lg border-l border-t border-b bg-lime-400 px-4 py-2">Availble</button>
+          <button className="rounded-r-lg border-r border-t border-b bg-white px-4 py-2">Selected</button>
         </div>
-    );
+      </div>
+      <div className="grid grid-cols-3 gap-8 mx-auto">
+        {players.map((player) => (
+          <Player key={player.playerId} player={player}></Player>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Players;
