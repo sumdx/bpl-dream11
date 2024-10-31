333bbebc01d4bcc96abf99cb0be92f119b0ab025
sumdx <sumdas74@gmail.com>
2024-10-30 19:23:25 -0700

    Footer Section Added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,17 +1,66 @@
-
+import "../../components/Footer/Footer.css";
+import Logo from "./../../images/logo-footer.png"
 
 const Footer = () => {
-    return (
-        <footer className="bg-gray-900">
-            <div className="container mx-auto]">
-                <h1>Bangladesh</h1>
+  return (
+    <footer className="bg-gray-900 relative pt-40 mt-48">
+      <div className="container mx-auto footer-float text-center py-20 border m-2 rounded-2xl flex flex-col gap-6 absolute left-0 right-0 -top-48">
+        <h1 className=" text-4xl font-bold">Subscribe to our Newsletter</h1>
+        <p>Get the latest updates and news right in your inbox!</p>
+        <div>
+          <input
+            className="px-4 py-2 rounded-lg border"
+            type="email"
+            placeholder="Enter Your Email"
+          />
+          <button className="ml-4 px-4 py-2 bg-black rounded-lg bg-gradient-r from-pink-800 to-orange-400 text-center text-white">
+            Subscibed
+          </button>
+        </div>
+      </div>
+      <img className="mx-auto mb-14" src={Logo} alt="" />
+      <div className="text-gray-400 ">
+        <div className="border-b border-gray-600 mb-4">
+          <div className="container grid grid-cols-3 mx-auto justify-evenly gap-20 mb-10">
+            <div>
+              <h1 className="text-white font-bold mb-4">About Us</h1>
+              <p>
+                We are a passionate team dedicated to providing the best
+                services to our customers.
+              </p>
             </div>
-            <div className="">
-                
+            <div>
+              <h1 className="text-white font-bold mb-4">Quick Links</h1>
+              <ul className="list-disc">
+                <li>Home</li>
+                <li>Services</li>
+                <li>About</li>
+                <li>Contact Us</li>
+              </ul>
             </div>
-        </footer>
+            <div>
+              <h1 className="text-white font-bold mb-4">About Us</h1>
+              <p>Subscribe to our newsletter for the latest updates.</p>
+              <div>
+                <input
+                  className="px-4 py-2 rounded-l-lg"
+                  type="email"
+                  placeholder="Enter Your Email"
+                />
+                <button className="px-4 py-2 border-none bg-green-400 text-center rounded-r-lg text-white">
+                  Subscibed
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
 
-    );
+        <p className="text-center pb-4">
+          @2024 Your Company All Rights Reserved.
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/components/Selections/Selections.jsx
+++ src/components/Selections/Selections.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Selections = ({availableBtnHandler,selectedBtnHandler,isActiveAvailable,isActiveSelected,cart}) => {
     return (
-        <div className="flex justify-between items-center my-8 container mx-auto">
+        <div className="flex justify-between items-center my-8 container mx-auto sticky top-0 bg-white py-4">
         <h1 className="font-bold text-xl">{isActiveAvailable? "Available Player": `Selected Player (${cart.length}/6)`}</h1>
         <div>
           <button onClick={()=>availableBtnHandler()} className={`rounded-l-lg border-l border-t border-b ${isActiveAvailable? "bg-lime-400" : "bg-white" }  px-4 py-2`}>Available</button>
